530e2fb1e6154d2111bf835acc4b11b7d592eef1
Tamim <tamim.365.ti@gmail.com>
2020-11-22 00:40:07 +0600

    single_blog_page Completed

--- js/main.js
+++ js/main.js
@@ -192,3 +192,10 @@ const showClass = "show";
 	});
 	
 })(jQuery);
+
+document.addEventListener( 'DOMContentLoaded', function () {
+	new Splide( '#image-slider', {
+		'cover'      : true,
+		'heightRatio': 0.5,
+	} ).mount();
+} );
